bbe6ad6dc77230ff4e467b92f60534fe3ddfed19
ArduinoCoderGuy <160273642+ArduinoCoderGuy@users.noreply.github.com>
2024-05-29 23:13:05 +0100

    Update SSD1306_DUAL_SCREEN.ino

--- SSD1306_DUAL_SCREEN.ino
+++ SSD1306_DUAL_SCREEN.ino
@@ -4,29 +4,37 @@
  Pick one up today in the adafruit shop!
  ------> http://www.adafruit.com/category/63_98
 
- This example is for a 128x32 pixel display using I2C to communicate
+ This example is for a 128x64 pixel display using I2C to communicate
  3 pins are required to interface (two I2C and one reset).
-
- Written by Philip Furness,.
+ This will run the snowflake program forever, just as a test.
+ 
+ Adapted by Philip Furness,.
  **************************************************************************/
-
+ 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define XPOS   0 // Indexes into the 'icons' array in function below
+#define YPOS   1
+#define DELTAY 2
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
 // On an arduino UNO: A4(SDA - I use a Yellow Wire), 
-//                    A5(SCL - I use a green Wire)
-#define OLED_RESET     -1 // Reset pin number (or -1 if sharing Arduino reset pin, which is what I do.)
-#define SCREEN_ADDRESS1 0x3C // Give Left screen a name & Set Address for Left Screen
-#define SCREEN_ADDRESS2 0x3D // Give Right Screen a name & Set Address for Right Screen
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//                    A5(SCL - I use a Green Wire)
 
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+Adafruit_SSD1306 display1(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display2(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
 static const unsigned char PROGMEM logo_bmp[] =
 { 0b00000000, 0b11000000,
   0b00000001, 0b11000000,
@@ -44,26 +52,104 @@ static const unsigned char PROGMEM logo_bmp[] =
   0b01111100, 0b11110000,
   0b01110000, 0b01110000,
   0b00000000, 0b00110000 };
-
-
+  
 void setup() {
   Serial.begin(9600);
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS1)) {
+  delay(2000);
+ display1.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Default OLED address.
+ display1.clearDisplay();
+ 
+ // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display1.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
-
   }
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS2)) {
+
+ 
+ display2.begin(SSD1306_SWITCHCAPVCC, 0x3D); // Second OLED address, solder onboard jumper
+ display2.clearDisplay();
+  
+ // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display2.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
-
   }
-  SCREEN_ADDRESS1.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  SCREEN_ADDRESS2.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  display1.display(); // Display the Adafruit Logo
+  display2.display(); // Display the Adafruit Logo
+  
+  delay(2000); 
+  display1.clearDisplay();
+  display2.clearDisplay();
+  
+  display1.setTextSize(1);
+  display1.setTextColor(WHITE);
+  display1.setCursor(0,0);
+  display1.print("Display A");
+  display1.display(); // Now Display "Display A"
+  
+  display2.setTextSize(1);
+  display2.setTextColor(WHITE);
+  display2.setCursor(0,0);
+  display2.print("Display B");
+  display2.display(); // Now Display "Display B"
+  
+  delay(2000); 
+
 }
+
 void loop() {
-  display.display();
-
+ delay(2000); 
+ testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps  
 }
+
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, icons[NUMFLAKES][3];
+
+  // Initialize 'snowflake' positions
+  for(f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display1.width());
+    icons[f][YPOS]   = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  for(;;) { // Loop forever...
+    display1.clearDisplay(); // Clear the display buffer
+    display2.clearDisplay(); // Clear the display buffer
+
+    // Draw each snowflake:
+    for(f=0; f< NUMFLAKES; f++) {
+      display1.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+      display2.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    }
+
+    display1.display(); // Show the display buffer on the screen
+    display2.display(); // Show the display buffer on the screen
+    delay(200);        // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for(f=0; f< NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display1.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display1.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+      
+      if (icons[f][YPOS] >= display2.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display2.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+    }
+  }
+}
+
